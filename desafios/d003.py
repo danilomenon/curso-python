6b65870c7748dad8e5f1ae4687c136afcc22f8a7
Danilo Menon <menondanilo153@gmail.com>
2025-01-17 14:09:48 -0300

    Resolução 'desafios/d003.py'

--- desafios/d003.py
+++ desafios/d003.py
@@ -1,1 +1,14 @@
 # Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ela.
+
+# Leitura de um valor pelo teclado
+valor = input("Digite algo: ")
+
+# Exibindo as informações sobre o valor digitado
+print("Tipo primitivo:", type(valor))
+print("É numérico?", valor.isnumeric())
+print("É alfabético?", valor.isalpha())
+print("É alfanumérico?", valor.isalnum())
+print("Está em maiúsculas?", valor.isupper())
+print("Está em minúsculas?", valor.islower())
+print("Está capitalizado?", valor.istitle())
+print("É um espaço em branco?", valor.isspace())
